4d3aaa82aa3eb1fe44d2ea5ddf3d6e90b862f566
barretoeanna <113685636+barretoeanna@users.noreply.github.com>
2022-09-16 07:38:27 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("vai começar")
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+})
+basic.forever(function () {
+	
+})
